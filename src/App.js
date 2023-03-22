a57524ef725695af6206a6cdbc042e163e75c817
IslemBacha <124149772+IslemBacha@users.noreply.github.com>
2023-03-22 20:24:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Formulws from './Component/Formulws';
+import Navbws from './Component/Navbws';
+function App() {
+  return (
+    <div>
+       <Navbws/>
+    <Formulws/>
+    
+    </div>
+    
+    
+    
+  );
+}
+
+export default App;
